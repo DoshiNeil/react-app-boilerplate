fc49f8a0ea79d32357e9b915e7c0d0b4d81b88a2
Neil Doshi <neil@nzymtechnologies.com>
2021-11-27 22:11:34 +0530

    tailwind template completed

--- src/components/CardComponent/index.js
+++ src/components/CardComponent/index.js
@@ -2,13 +2,10 @@ import React from 'react';
 
 const Card = (props) => {
     return (
-        <div className="card">
-            <div className="card-body">
-                <h5 className="card-title">{props.user.name}</h5>
-                <h6 className="card-subtitle mb-2 text-muted">{props.user.company.name}</h6>
-                <p className="card-text">{props.user.company.catchPhrase}</p>
-            </div>
-            
+        <div className="max-wd-sm rounded overflow shadow-md mb-5 p-2 border">
+                <h5 className="font-bold text-xl">{props.user.name}</h5>
+                <h6 className="text-gray-500 mb-2 text-sm">{props.user.company.name}</h6>
+                <p className="text-gray-700 text-base">{props.user.company.catchPhrase}</p>
         </div>
     );
 }
